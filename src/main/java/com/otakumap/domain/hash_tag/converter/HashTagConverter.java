be2eb97fe959c6d225f1b127c702e97f8166350c
mk-star <ssunn0812@naver.com>
2025-02-16 17:16:44 +0900

    Feat: 애니메이션 저장 시 해시태그도 같이 저장

--- src/main/java/com/otakumap/domain/hash_tag/converter/HashTagConverter.java
+++ src/main/java/com/otakumap/domain/hash_tag/converter/HashTagConverter.java
@@ -1,13 +1,29 @@
 package com.otakumap.domain.hash_tag.converter;
 
+import com.otakumap.domain.animation.entity.Animation;
 import com.otakumap.domain.hash_tag.dto.HashTagResponseDTO;
 import com.otakumap.domain.hash_tag.entity.HashTag;
+import com.otakumap.domain.mapping.AnimationHashtag;
 
 public class HashTagConverter {
+    public static HashTag toHashTag(String name) {
+        return HashTag.builder()
+                .name("#" + name)
+                .build();
+
+    }
+
     public static HashTagResponseDTO.HashTagDTO toHashTagDTO(HashTag hashTag) {
         return HashTagResponseDTO.HashTagDTO.builder()
                 .hashTagId(hashTag.getId())
                 .name(hashTag.getName())
+                .build();
+    }
+
+    public static AnimationHashtag toAnimationHashTag(Animation animation, HashTag hashTag) {
+        return AnimationHashtag.builder()
+                .animation(animation)
+                .hashTag(hashTag)
                 .build();
     }
 }
